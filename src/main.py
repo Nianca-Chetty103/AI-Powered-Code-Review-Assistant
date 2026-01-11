2c72f1a7f5bc51c3341f074498732d5cd461e181
Nianca Chetty <niancachetty102@gmail.com>
2026-01-11 15:57:08 +0200

    Add add_numbers function and update print statement

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print("Hello world from src folder")
+def add_numbers(a, b):
+    c = a + b
+    return c
+
+def unused_function():
+    x = 10  # pylint should warn about this
+
+print(add_numbers(2, 3))
+
